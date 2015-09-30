e9ea4c983cb2698befa37654b036e0953636231e
Gene Shuman <gene@scribd.com>
2015-09-30 16:15:31 -0700

    edge object done

--- model/edge.py
+++ model/edge.py
@@ -1,28 +1,53 @@
+import math
+
 class Edge(object):
-    """ """
+    """ A generic representation of a two dimensional line segment of variable type """
     def __init__(self, vertices):
         self.vertices = vertices
 
     def arc_length(self):
-        pass
+        raise NotImplementedError("subclass responsibly")
 
     def __repr__(self):
         return "V:%s" % str(self.vertices)
 
 
 class LinearEdge(Edge):
-    """ """
+    """ A representation of a two dimensional linear line segment """
     def __init__(self, vertices):
+        assert len(vertices) == 2
+
         super(LinearEdge, self).__init__(vertices)
 
     def arc_length(self):
-        1.0
+        return math.sqrt((self.vertices[0].x - self.vertices[1].x) ** 2 +
+                         (self.vertices[0].y - self.vertices[1].y) ** 2)
 
 
 class CircularEdge(Edge):
-    """ """
+    """ A representation of a two dimensional circular arc
+    Either a 1-verticed circle, or a 2-verticed arc
+    moving clockwise from v[0] -> v[1]
+    """
     def __init__(self, vertices, center):
+        assert len(vertices) == 1 or len(vertices) == 2
+
+        self.center = center
         super(CircularEdge, self).__init__(vertices)
 
     def arc_length(self):
-        1.0
+        r = LinearEdge([self.vertices[0], self.center]).arc_length()
+        if(len(self.vertices) == 1):
+            return 2.0 * math.pi * r
+        else:
+            v0 = (self.vertices[0].x - self.center.x, self.vertices[0].y - self.center.y)
+            v1 = (self.vertices[1].x - self.center.x, self.vertices[1].y - self.center.y)
+            dp = v0[0] * v1[0] + v0[1] * v1[1]
+            th = math.acos(dp / (r * r))
+
+            if(v1[1] * v0[0] <= v0[1] * v1[0]):
+                print 'o'
+                th = 2 * math.pi - th
+
+            print dp, th
+            return r * th
--- model/test_model.py
+++ model/test_model.py
@@ -1,13 +1,55 @@
-import pytest
+import pytest, math
 
 from point import *
 from edge  import *
 from model import *
 
-def test_default():
-    vertices = [Point(0,0), Point(0,1), Point(1,1), Point(1,0)]
-    edges = [Edge([vertices[0], vertices[1]]), Edge([vertices[1], vertices[2]]), Edge([vertices[2], vertices[3]]), Edge([vertices[3], vertices[0]])]
-    model = Model(edges)
 
-    print model
-    assert True == False
+def test_linear_edge_length():
+    vertices = [Point(0,0), Point(0,1), Point(1,1), Point(1,0)]
+    edges = [LinearEdge([vertices[0], vertices[1]]), LinearEdge([vertices[1], vertices[2]]), LinearEdge([vertices[2], vertices[3]]), LinearEdge([vertices[3], vertices[0]])]
+
+    assert [edge.arc_length() for edge in edges] == [1.0, 1.0, 1.0, 1.0]
+
+
+def test_circular_edge_length():
+    # single vertex
+    edge = CircularEdge([Point(0,1)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi ) < 0.0000001
+
+    edge = CircularEdge([Point(0,2)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi * 2.0 ) < 0.0000001
+
+    # double vertex
+    edge = CircularEdge([Point(0,1), Point(0,1)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi ) < 0.0000001
+
+    edge = CircularEdge([Point(1,0), Point(0,1)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(0,1), Point(1,0)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi * 3 / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(0,1), Point(-1,0)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(-1,0), Point(0,1)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi * 3 / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(1,0), Point(-1,0)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * math.pi * 2 / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(1,0), Point(-0.9,0.1)], Point(0,0))
+    assert edge.arc_length() < math.pi
+
+    edge = CircularEdge([Point(1,0), Point(-0.9,-0.1)], Point(0,0))
+    assert edge.arc_length() > math.pi
+
+    edge = CircularEdge([Point(0,2), Point(0,-2)], Point(0,0))
+    assert abs(edge.arc_length() - 2.0 * 2.0 * math.pi * 2 / 4 ) < 0.0000001
+
+    edge = CircularEdge([Point(0,2), Point(-0.1,-0.9)], Point(0,0))
+    assert edge.arc_length() < 2.0 * math.pi
+
+    edge = CircularEdge([Point(0,2), Point(0.1,-0.9)], Point(0,0))
+    assert edge.arc_length() > 2.0 * math.pi
