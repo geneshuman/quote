8badc8437bcf944bc7fb1fa4894f7114194d1121
Gene Shuman <gene@scribd.com>
2015-09-30 18:01:42 -0700

    model.bounding_area

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from edge import *
+
 class Model(object):
     """ A representaion of a basic 2d object in terms of sequential edges """
 
@@ -8,8 +10,12 @@ class Model(object):
         return "E:%s" % str(self.edges)
 
     def bounding_rectangle(self):
-        ''' complicated, will do later '''
+        ''' assume rectangle for now '''
+        return [edge.vertices[0] for edge in self.edges]
 
     def bounding_area(self, pad=0.0):
-        ''' complicated, will do later '''
-        return 1.0
+        ''' computes the area of the bounding rectangle with padding '''
+        rect = self.bounding_rectangle()
+        w = LinearEdge([rect[0], rect[1]]).arc_length()
+        h = LinearEdge([rect[1], rect[2]]).arc_length()
+        return (w + 2.0 * pad) * (h + 2.0 * pad)
